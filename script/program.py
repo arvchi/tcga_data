12f822a4a21c99b6d708e1dec92bfd5decbfc848
Arvin C <arvin.chireh@gmail.com>
2018-10-05 12:44:53 +0200

    fixed data types to numeric

--- script/program.py
+++ script/program.py
@@ -3,7 +3,16 @@ from lifelines import CoxPHFitter
 
 df = pd.read_csv("../data/data.txt",  sep='\t', index_col=0, header=None)
 dft = df.transpose()
-dft = dft.iloc[:,4:]  # remove non-numeric columns
+
+d = {'dead': True, 'alive': False}
+dft["Status"] = dft["Status"].map(d) # make event boolean
+
+dft = dft.iloc[:,4:]  # only transcriptomic data as indepenent variablea
+dft = dft.drop("Age",1)
+
+# Make numeric (except event, which is boolean)
+dft = dft.apply(pd.to_numeric, errors='coerce')
 
 cph = CoxPHFitter()
-cph.fit(df1, duration_col='LivingDays', event_col='Status', show_progress=True)
+cph.fit(dft, duration_col='LivingDays', event_col='Status', show_progress=True)
+
